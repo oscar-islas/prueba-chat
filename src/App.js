b289626c81bdc7809807dd84a280e83dbb529bc9
Monicavila <di.monicavila@gmail.com>
2020-12-14 20:42:26 -0600

    reactRouterFisrtProposal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Router, Route, Switch } from "react-router";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar.jsx";
@@ -8,20 +8,22 @@ import Register from "./components/Register/Register.jsx";
 import ProtectedComponent from "./components/ProtectedComponent/ProtectedComponent";
 
 const App = () => (
-  <Router>
-    <Route exact path="/login" component={LogIn} />
-    <Route exact path="/register" component={Register} />
-    <Switch>
-      <ProtectedComponent>
-        <NavBar />
-        <Route exact path="/chat" component={Home} />
-      </ProtectedComponent>
-      <ProtectedComponent>
-        <NavBar />
-        <Route exact path="/chat/:id" component={Home} />
-      </ProtectedComponent>
-    </Switch>
-  </Router>
+  <Suspense fallback="Cargando para comenzar El Cotorreo... ">
+    <Router>
+      <Route exact path="/login" component={LogIn} />
+      <Route exact path="/register" component={Register} />
+      <Switch>
+        <ProtectedComponent>
+          <NavBar />
+          <Route exact path="/chat" component={Home} />
+        </ProtectedComponent>
+        <ProtectedComponent>
+          <NavBar />
+          <Route exact path="/chat/:id" component={Home} />
+        </ProtectedComponent>
+      </Switch>
+    </Router>
+  </Suspense>
 );
 
 export default App;
--- src/config/routing/routing/paths.js
+++
@@ -1,4 +0,0 @@
-export const HOME = "/chat";
-export const HOME = "/chat/:id";
-export const LOGIN = "/login";
-export const REGISTER = "/private-view";
